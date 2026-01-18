4b3f9f5d7c30fa2554d2c0812f430fd92daca35a
ichmy55 <ichmy@nifty.com>
2026-01-18 22:14:15 +0900

    Zoomit のダウンロード#8

--- task/sandbox/variables.mk
+++ task/sandbox/variables.mk
@@ -1,5 +1,6 @@
 #
 # ダウンロード先URL
 #
-UWSCR_URL := "https://github.com/stuncloud/UWSCR/releases/download/1.1.6.1/UWSCRx64.zip"
-PPINK_URL := "https://github.com/pubpub-zz/ppInk/releases/download/v1.9.100/ppInk.1.9.100.zip"
+UWSCR_URL  := "https://github.com/stuncloud/UWSCR/releases/download/1.1.6.1/UWSCRx64.zip"
+PPINK_URL  := "https://github.com/pubpub-zz/ppInk/releases/download/v1.9.100/ppInk.1.9.100.zip"
+ZOOMIT_URL := "https://download.sysinternals.com/files/ZoomIt.zip"
